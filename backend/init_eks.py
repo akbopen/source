c155d42aec6916d6707f983c0714217ba359dbdb
Shell2 <sunhaili@gmail.com>
2021-03-14 20:16:47 +1100

    Update init_eks.py

--- backend/init_eks.py
+++ backend/init_eks.py
@@ -2,16 +2,18 @@
 # https://pypi.org/project/aws-cdk.aws-eks/
 
 
-#step 1.1 Create an Amazon VPC with public and private subnets that meets Amazon EKS requirements.
+def create_aws_vpc():
+  """ #step 1.1 Create an Amazon VPC with public and private subnets that meets Amazon EKS requirements.
 
-```
-aws cloudformation create-stack \
-  --stack-name airformex-eks-vpc-stack \
-  --template-url https://s3.us-west-2.amazonaws.com/amazon-eks/cloudformation/2020-10-29/amazon-eks-vpc-private-subnets.yaml
-```
+  ```
+  aws cloudformation create-stack \
+    --stack-name airformex-eks-vpc-stack \
+    --template-url https://s3.us-west-2.amazonaws.com/amazon-eks/cloudformation/2020-10-29/amazon-eks-vpc-private-subnets.yaml
+  ```
+  """
+  pass
   
-  
- #step 1.2
+#step 1.2
 
 
 #step 1.3
