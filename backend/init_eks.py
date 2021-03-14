bb6c9b74aec1399fee467f2533af2f0f16bb023f
Bruce <ximai.liu@gmail.com>
2021-03-14 20:14:23 +1100

    Update init_eks.py

--- backend/init_eks.py
+++ backend/init_eks.py
@@ -1,1 +1,7 @@
 # init eks
+
+
+#step 1.1 Create an Amazon VPC with public and private subnets that meets Amazon EKS requirements.
+aws cloudformation create-stack \
+  --stack-name airformex-eks-vpc-stack \
+  --template-url https://s3.us-west-2.amazonaws.com/amazon-eks/cloudformation/2020-10-29/amazon-eks-vpc-private-subnets.yaml
